b297186d14ffb3afa951c955539b6a1970fd9fe7
alan <alan200994@gmail.com>
2017-01-24 13:38:58 -0800

    build scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('move', function() {
   gulp.src(['node_modules/normalize.css/normalize.css', 'node_modules/vue/dist/vue.min.js'])
   .pipe(gulp.dest('assets/vendor'));
+});
+
+gulp.task('build:scss', function() {
+  gulp.src('src/scss/**')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('assets/css'));
+});
+
+
+// --- WORK FLOW
+gulp.task('build', ['move', 'build:scss'], function() {});
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**', ['build:scss']);
 });
