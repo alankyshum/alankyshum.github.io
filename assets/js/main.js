83819fd3332156ea1dff58f03778de78bef0872e
Alan Shum <alan200994@gmail.com>
2017-02-13 23:50:01 -0800

    added all content

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,18 +43,70 @@ const DATA = {
       logo: 'assets/img/logos/HKUST_Logo.svg',
       institution: 'The Hong Kong University of Science and Technology (HKUST)',
       time: '2012-2016',
+      programme: 'BEng of Computer Science',
       details: {
-        CGA: '3.132/4.3',
-        Major: 'BEng of Computer Science'
+        CGA: '3.132/4.3'
       }
     }, {
       logo: 'assets/img/logos/KAIST.png',
+      logoStyle: 'width: 70%',
       institution: 'The Korea Advanced Insitute of Science and Technology (KAIST)',
       time: 'Jan 2015 - Jun 2015',
+      programme: 'Exchange Programme',
       details: {
         TGA: '3.3/4.3',
         Programmes: 'Studied: 2-D Design, Computer Network, Financial Management, Basic Korean I for Foreign students'
       }
+    }],
+    recognitions: [{
+      time: 2014,
+      logo: 'assets/img/logos/HackUST.png',
+      logoStyle: 'width: 70%',
+      award: 'Audience Favourite Award (Team Award)',
+      organiser: 'HackUST 2014, THe HKUST',
+      details: 'Openlore – A project made for Hackathon competition held in the HKUST, aiming at providing an all-in-one solution for online-learning and course content management.'
+    }, {
+      time: 2012,
+      logo: 'assets/img/logos/HKUST_Logo.svg',
+      award: 'School of Engineering Scholarship and University Scholarship',
+      organiser: 'The HKUST',
+      details: 'Award to student in recognition to their outstanding performance in HKDSE public examination.'
+    }],
+    works: [{
+      title: 'Full-stack web developer',
+      time: 'Aug 2016 – Present',
+      company: 'Startup Compass Inc., San Francisco, the USA',
+      duties: 'Responsible for building the benchmarking platform from front-end (Angular.JS) and back-end side (Ruby + Postgresql).'
+    }, {
+      title: 'Full-stack web developer',
+      time: 'Jun 2016 – Jul 2016',
+      company: 'Fanswifi., Hong Kong',
+      duties: 'Contributed to building social login feature, from both front-end and back- end side (Node.JS in ES6 for APIs+ PHP for static assets server).'
+    }, {
+      title: 'Front-end developer (Remote)',
+      time: 'Aug 2015 – May 2016',
+      company: 'Startup Compass Inc., San Francisco, the USA',
+      duties: 'Responsible for building the front-end of the SAAS website of the company.'
+    }, {
+      title: 'Web developer trainee',
+      time: 'July 2015 – Aug 2015',
+      company: 'Startup Compass Inc., San Francisco, the USA',
+      duties: 'Responsible for building the front-end of the SAAS website of the company.'
+    }, {
+      title: 'Software Engineer Intern (Part-time)',
+      time: 'Sep 2015 – Nov 2015',
+      company: 'Keptme, HK',
+      duties: 'Responsible for front-end development of the web platform.'
+    }, {
+      title: 'July 2012 – May 2014',
+      time: 'Technical Support Role',
+      company: 'Omnitech Info Solutions (Career Works), HK',
+      duties: 'Provide technical support services as a helpdesk role and standby for solving technical issues encountered by the staff workers of the investment banks we serve.'
+    }, {
+      title: 'July 2013 – August 2013',
+      time: 'Network Operation Engineer (Internship)',
+      company: 'PCCW, HK',
+      duties: 'Assist in providing programming support of the Web server set up, web portal modification and web application programming.'
     }]
   }
 };
